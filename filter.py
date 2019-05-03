08627278eae5199a6a5a3eff6ef9572d17aa55dd
470508382 <470508382@qq.com>
2019-05-04 03:13:51 +0800

    运行完admission_filter()发现不是EV的病人只有17000人，是E的有22000人，是EV的有97000人，故应该对不是EV的进行筛选。但时间复杂度还是很高，因为经过计算labevents表有2700w行，删掉空行还有2200w行，应对病人进行进一步筛选（根据项目）

--- filter.py
+++ filter.py
@@ -17,39 +17,168 @@ def labevents_abnormal():
 #                    print(row['ROW_ID'])
 #                    print("\n")
 
+def in_HADM(hadm_id):
+    """
+    判断是否在HADM_ID_EV表里
+    """
+    path = r'C:\Users\47050\Desktop\data\HADM_ID_EV.csv'
+    with open(path, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if(row['HADM_ID']==hadm_id):
+                return 1
+            else:
+                continue
+        return 0
 
 def labevents_filter():
     """
-    对labevents_data_table的处理，HADM_ID为空的删除，因为诊断都是对住院病人进行的诊断
+    对labevents_data_table的处理，HADM_ID为空的删除,in_HADM的删除
     保存的字段有：SUBJECT_ID、HADM_ID、ITEMID、CHARTTIME、VALUE、VALUENUM、VALUEUOM、FLAG
     是否可以每个HADM_ID代表一个新病人，删除SUBJECT_ID字段？
     """
+    num = 0     #计数 看运行到哪了
     with open(r'C:\Users\47050\Desktop\data\labevents_filter.csv', 'w') as fout:
-        fout.write('SUBJECT_ID' + ',' + 'HADM_ID' + ',' + 'ITEMID' + ',' + 'CHARTTIME' + ','
-                   + 'VALUE' + ',' + 'VALUENUM' + ',' + 'VALUEUOM,FLAG' + '\n')
+        fout.write('SUBJECT_ID,HADM_ID,ITEMID,CHARTTIME,VALUE，VALUENUM，VALUEUOM,FLAG\n')
+        fin = r'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\LABEVENTS_DATA_TABLE.csv'
+        with open(fin, 'r') as fin:
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if(len(row['HADM_ID'])>0 and in_HADM(row['HADM_ID'])==0):
+                    if not(num == 0):
+                        fout.write('\n')
+                    fout.write(row['SUBJECT_ID'] + ',' + row['HADM_ID'] + ',' + row['ITEMID'] + ',' + row['CHARTTIME']
+                               + ',' + row['VALUE'] + ',' + row['VALUENUM'] + ',' + row['VALUEUOM'] + ','+row['FLAG'])
+                    num = num+1
+                    print(num)
+                    print("\n")
+
+
+def hadm_id_EV():
+    """
+    ICD9_CODE 中E开头的为意外事故（车祸、割腕等）可以删除
+    ICD9_CODE 中V开头的也删除
+    将ICD9_CODE中以E或V开头的HADM_ID保存成新表
+    之后可以对admissions表、diagnoses表、d_diagnoses表进行处理，
+    """
+    num = 0
+    finpath = 'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\DIAGNOSES_ICD_DATA_TABLE.csv'
+    foutpath = r'C:\Users\47050\Desktop\data\HADM_ID_EV.csv'
+    with open(finpath, 'r') as fin:    #读diagnoses表
+        with open(foutpath, 'w') as fout:
+            fout.write('HADM_ID')
+            fout.write("\n")
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if(row['ICD9_CODE'].startswith('E') or row['ICD9_CODE'].startswith('V')):
+                    if(not(num == 0)):
+                        fout.write('\n')
+                    fout.write(row['HADM_ID'])
+                    num = num + 1
+
+
+def admission_filter():
+    """
+    在ICD9_filter1()中生成的csv文档中循环 更新admissions表
+    admission表保留SUBJECT_ID,HADM_ID,ADMITTIME,DISCHTIME,ADMISSION_TYPE,DIAGNOSIS
+    """
+    num = 0
+    fin_path = 'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\ADMISSIONS_DATA_TABLE.csv'
+    fout_path = r'C:\Users\47050\Desktop\data\ADMISSIONS_ICD_filter.csv'
+    with open(fout_path, 'w') as fout:
+        fout.write('SUBJECT_ID,HADM_ID,ADMITTIME,DISCHTIME,ADMISSION_TYPE,DIAGNOSIS\n')
+        with open(fin_path, 'r') as fin:
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if(in_HADM(row['HADM_ID']) == 0):
+                    if(not(num == 0)):
+                        fout.write('\n')
+                    fout.write(row['SUBJECT_ID']+','+row['HADM_ID']+','+row['ADMITTIME']+',')
+                    fout.write(row['DISCHTIME'] + ',' + row['ADMISSION_TYPE'] + ',' + row['DIAGNOSIS'])
+                    num = num + 1
+                    print(num)
+                    print('\n')
+
+
+def diagnoses_filter():
+    """
+    在ICD9_filter1()中生成的csv文档中循环 更新diagnoses_icd_data表
+    diagnose表保留SUBJECT_ID	,HADM_ID,SEQ_NUM,ICD9_CODE
+    """
+    num = 0
+    fin_path = 'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\DIAGNOSES_ICD_DATA_TABLE.csv'
+    fout_path = r'C:\Users\47050\Desktop\data\DIAGNOSES_ICD_filter.csv'
+
+    with open(fout_path, 'w') as fout:
+        fout.write('SUBJECT_ID,HADM_ID,SEQ_NUM,ICD9_CODE\n')
+        with open(fin_path, 'r') as fin:
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if(in_HADM(row['HADM_ID'])==0):
+                    if(not(num == 0)):
+                        fout.write('\n')
+                    fout.write(row['SUBJECT_ID']+','+row['HADM_ID']+',')
+                    fout.write(row['SEQ_NUM'] + ',' + row['ICD9_CODE'])
+                    num = num + 1
+                    print(num)
+                    print('\n')
+
+def count():
+    num = 0
+    fin_path = r'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\LABEVENTS_DATA_TABLE.csv'
+    with open(fin_path, 'r') as fin:
+        reader = csv.DictReader(fin)
+        for row in reader:
+            num = num + 1
+    print(num)
+
+
+def empty_filter():
+    """
+    把labevents表中没有住院号的删掉
+    """
+    num = 0     #计数 看运行到哪了
+    with open(r'C:\Users\47050\Desktop\data\labevents_empty_filter.csv', 'w') as fout:
+        fout.write('SUBJECT_ID,HADM_ID,ITEMID,CHARTTIME,VALUE，VALUENUM，VALUEUOM,FLAG\n')
         fin = r'D:\BaiduNetdiskDownload\MIMIC_III\MIMIC_III\LABEVENTS_DATA_TABLE.csv'
         with open(fin, 'r') as fin:
             reader = csv.DictReader(fin)
             for row in reader:
                 if not(len(row['HADM_ID']) == 0):
+                    if not(num == 0):
+                        fout.write('\n')
                     fout.write(row['SUBJECT_ID'] + ',' + row['HADM_ID'] + ',' + row['ITEMID'] + ',' + row['CHARTTIME']
                                + ',' + row['VALUE'] + ',' + row['VALUENUM'] + ',' + row['VALUEUOM'] + ','+row['FLAG'])
+                    num = num+1
+                    if(num % 1000000 == 0):
+                        print(num)
+
+
+
+def hadm_id_notEV():
+    """
+    遍历admission_filter表 把ham_id保存
+    """
+    num = 0
+    finpath = r'C:\Users\47050\Desktop\data\ADMISSIONS_ICD_filter.csv'
+    foutpath = r'C:\Users\47050\Desktop\data\HADM_ID_notEV.csv'
+    with open(finpath, 'r') as fin:
+        with open(foutpath, 'w') as fout:
+            fout.write('HADM_ID\n')
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if not(num == 0):
                     fout.write('\n')
-#                    print(row['ROW_ID'])
-#                    print("\n")
+                fout.write(row['HADM_ID'])
+                num = num + 1
 
 
-def diagnoses_icd_filter():
-    """
-    三个表可以一起删
-    ICD9_CODE 中E开头的为意外事故（车祸、割腕等）可以删除
-    之后可以对admissions表中 每个HAMD_ID去diagnoses_icd_data表中找，
-    若ICD_CODE不是以E或V开头（即是数字）则保存到新表
-    """
-
-
+#count()
+#empty_filter()
+#hadm_id_notEV()
 
 #labevents_abnormal()
+#hadm_id_EV()
 #labevents_filter()
 
 
